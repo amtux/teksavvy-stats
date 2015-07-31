10b5c775602972c5de9da3a78b7dfd8c9bcd925e
amtux <aman.singh.uppal@gmail.com>
2015-07-31 16:08:23 -0400

    finished monthly view

--- app/scripts/controllers/monthly.js
+++ app/scripts/controllers/monthly.js
@@ -8,20 +8,59 @@
  * Controller of the teksavvyStatsApp
  */
 angular.module('teksavvyStatsApp')
-  .controller('MonthlyCtrl', function () {
+  .controller('MonthlyCtrl', function($scope, $http, $cookies) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
+    $scope.xFunction = function() {
+      return function(d) {
+        return d.key;
+      };
+    };
+    $scope.yFunction = function() {
+      return function(d) {
+        return d.y;
+      };
+    };
+
+    $scope.displayData = [];
+
     if ($cookies.get('TekSavvy-APIKey')) {
       $scope.showKey = true;
       $scope.apiKeyValue = $cookies.get('TekSavvy-APIKey');
+
+      var url = '//localhost:3000/summary/' + $scope.apiKeyValue;
+      $http.get(url)
+        .success(function(data) {
+          $scope.monthlyData = data.value;
+          console.log($scope.monthlyData);
+
+          for (var i = 0; i < $scope.monthlyData.length; i++) {
+            var temp = [{
+              key: "On Peak Upload (GB)",
+              y: $scope.monthlyData[i].OnPeakUpload
+            }, {
+              key: "On Peak Download (GB)",
+              y: $scope.monthlyData[i].OnPeakDownload
+            }, {
+              key: "Off Peak Upload (GB)",
+              y: $scope.monthlyData[i].OffPeakUpload
+            }, {
+              key: "Off Peak Download (GB)",
+              y: $scope.monthlyData[i].OffPeakDownload
+            }];
+            $scope.displayData.push(temp);
+          }
+
+        })
+        .error(function(status) {
+          console.log('Error validating key via GET on: ' + url + ', status: ' + status);
+        });
     } else {
       $scope.showKey = false;
     }
-
-    if value.
 
   });
